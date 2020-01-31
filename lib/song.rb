872cee211af33b8916cd28fcec4437dd26af52b9
Chris Wade <swade781977@yahoo.com>
2020-01-31 00:42:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,8 +31,7 @@ class Song
     self.all.find{|song| song.name == name} 
   end
   def self.find_or_create_by_name(name)
-    
-    end
+    find_by_name(name) || create_by_name(name)
   end
     
 end
